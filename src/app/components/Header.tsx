78a34c6c233d07ff56da633c1a628ff37fcd9c0d
moguDev <mail@sho-horiguchi.jp>
2024-10-26 21:55:19 +0900

    Headerファイルの作成

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="fixed top-0 h-16 w-full bg-main">
+      <div className="flex items-center p-5">
+        <h1 className="text-base font-black text-2xl">flully</h1>
+      </div>
+    </header>
+  );
+};
