059994b2db3555fbcb27b4be8f5665e07d97a541
moguDev <mail@sho-horiguchi.jp>
2024-10-30 00:53:33 +0900

    カードメニューの詳細作成など

--- src/app/signup/components/SignupForm.tsx
+++ src/app/signup/components/SignupForm.tsx
@@ -1,12 +1,44 @@
 "use client";
 
+import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+};
 
 export const SignupForm = () => {
   const router = useRouter();
+  const { signup } = useAuth();
+  const defaultValues: FormData = {
+    name: "",
+    nickname: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const onsubmit = async (data: FormData) => {
+    await signup(data).then(() => router.back());
+  };
+
   return (
     <div>
-      <form method="post" className="space-y-4">
+      <form
+        method="post"
+        onSubmit={handleSubmit(onsubmit)}
+        className="space-y-4"
+      >
         <div className="mt-3">
           <label htmlFor="flully-id" className="font-bold text-xs text-main">
             flully ID
@@ -19,7 +51,21 @@ export const SignupForm = () => {
               type="text"
               className="w-full bg-base outline-none"
               placeholder="半角英数字4文字以上"
+              {...register("name", {
+                required: "flully IDを入力してください",
+                minLength: {
+                  value: 4,
+                  message: "flully IDは4文字以上にしてください。",
+                },
+                maxLength: {
+                  value: 24,
+                  message: "flully IDは24文字以内にしてください。",
+                },
+              })}
             />
+          </div>
+          <div className="text-red-500 font-bold text-xs p-1">
+            {errors.name?.message}
           </div>
         </div>
         <div>
@@ -32,7 +78,17 @@ export const SignupForm = () => {
               type="text"
               className="w-full bg-base outline-none"
               placeholder="アカウント名"
+              {...register("nickname", {
+                required: "アカウント名を入力してください。",
+                maxLength: {
+                  value: 32,
+                  message: "アカウント名は32文字以内にしてください。",
+                },
+              })}
             />
+          </div>
+          <div className="text-red-500 font-bold text-xs p-1">
+            {errors.nickname?.message}
           </div>
         </div>
         <div>
@@ -45,7 +101,17 @@ export const SignupForm = () => {
               type="email"
               className="w-full bg-base outline-none"
               placeholder="example@flully.jp"
+              {...register("email", {
+                required: "メールアドレスを入力してください。",
+                pattern: {
+                  value: /^[a-zA-Z\d._%+-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/i,
+                  message: "メールアドレスの形式が不正です。",
+                },
+              })}
             />
+          </div>
+          <div className="text-red-500 font-bold text-xs p-1">
+            {errors.email?.message}
           </div>
         </div>
         <div>
@@ -58,7 +124,17 @@ export const SignupForm = () => {
               type="password"
               className="w-full bg-base outline-none"
               placeholder="半角英数字8文字以上"
+              {...register("password", {
+                required: "パスワードを入力してください。",
+                minLength: {
+                  value: 8,
+                  message: "パスワードは8文字以上にしてください。",
+                },
+              })}
             />
+          </div>
+          <div className="text-red-500 font-bold text-xs p-1">
+            {errors.password?.message}
           </div>
         </div>
         <div>
@@ -71,7 +147,13 @@ export const SignupForm = () => {
               type="password"
               className="w-full bg-base outline-none"
               placeholder="半角英数字8文字以上"
+              {...register("passwordConfirmation", {
+                required: "パスワード（確認用）を入力してください。",
+              })}
             />
+          </div>
+          <div className="text-red-500 font-bold text-xs p-1">
+            {errors.passwordConfirmation?.message}
           </div>
         </div>
         <button
