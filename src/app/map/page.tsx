03b9195cd9dc007b3fd88d267e7e17dd2090d6d2
moguDev <mail@sho-horiguchi.jp>
2024-12-06 23:02:26 +0900

    掲示板のエラー修正

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,7 @@
+import { Metadata } from "next";
 import { Map } from "./components/Map";
 import { PostDeleteModal } from "./components/PostDeleteModal";
 import { PostModal } from "./components/PostModal";
-import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "ホーム | flully - ふらりと出会った動物をシェアできるコミュニティ",
