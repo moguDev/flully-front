b0d889e4fbb8d91f8152b95864ce03b39f4ec58a
moguDev <mail@sho-horiguchi.jp>
2024-12-06 20:42:20 +0900

    掲示板の動的OGPの作成・設定

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -1,4 +1,50 @@
+import { Metadata } from "next";
 import { BoardDetail } from "./components/BoardDetails";
+import { api } from "@/lib/axiosInstance";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const { id } = params;
+  const res = await api.get(`/boards/${id}`);
+  const recipe = res.data;
+
+  return {
+    title: "flully - ふらりと出会った動物をシェアできるコミュニティ",
+    description: `ふらりは、街や自然で出会った動物をシェアできるコミュニティサービスです。
+      近所で見つけた鳥や猫、犬などを投稿して、他の動物好きなユーザーとシェアしましょう。
+      さらに、迷子のペットや保護したペットの情報を共有することもできます。
+      迷子のペットと飼い主がいち早く再開できるようにサポートします。`,
+    openGraph: {
+      title: "flully - ふらりと出会った動物をシェアできるコミュニティ",
+      description: `ふらりは、街や自然で出会った動物をシェアできるコミュニティサービスです。
+      近所で見つけた鳥や猫、犬などを投稿して、他の動物好きなユーザーとシェアしましょう。
+      さらに、迷子のペットや保護したペットの情報を共有することもできます。
+      迷子のペットと飼い主がいち早く再開できるようにサポートします。`,
+      url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/boards/${id}`,
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/boards/${id}/opengraph-image`,
+          width: 1200,
+          height: 630,
+          alt: recipe?.title,
+        },
+      ],
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "flully - ふらりと出会った動物をシェアできるコミュニティ",
+      description: `ふらりは、街や自然で出会った動物をシェアできるコミュニティサービスです。
+      近所で見つけた鳥や猫、犬などを投稿して、他の動物好きなユーザーとシェアしましょう。
+      さらに、迷子のペットや保護したペットの情報を共有することもできます。
+      迷子のペットと飼い主がいち早く再開できるようにサポートします。`,
+      images: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/boards/${id}/opengraph-image`,
+    },
+  };
+}
 
 export default function BoardDetailsPage() {
   return (
